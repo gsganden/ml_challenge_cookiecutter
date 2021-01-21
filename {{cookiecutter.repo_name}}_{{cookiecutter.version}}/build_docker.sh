d961f81fcad162cf411a209d0f46f62866b66539
gsganden <gsganden@gmail.com>
2021-01-21 09:03:36 -0600

    Compile reqs when building container

--- {{cookiecutter.repo_name}}_{{cookiecutter.version}}/build_docker.sh
+++ {{cookiecutter.repo_name}}_{{cookiecutter.version}}/build_docker.sh
@@ -1,1 +1,3 @@
-docker build . -t {{cookiecutter.repo_name}}:{{cookiecutter.version}}
+pip-compile \
+&& pip-compile requirements-dev.in \
+&& docker build . -t {{cookiecutter.repo_name}}:{{cookiecutter.version}}
